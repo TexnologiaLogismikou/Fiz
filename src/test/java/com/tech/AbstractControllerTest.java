52981b1e1fff048c5ee4cab4ac333836b515a5be
newbi12 <shinigamidxd@gmail.com>
2015-11-17 16:10:36 +0200

    Full working test for UserService User Profile updated. HalfWorking

---
+++ src/test/java/com/tech/AbstractControllerTest.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tech;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
+/**
+ *
+ * @author KuroiTenshi
+ */
+@WebAppConfiguration
+public abstract class AbstractControllerTest extends AbstractTest{
+    
+    protected MockMvc mvc;
+    
+    @Autowired
+    protected WebApplicationContext webApplicationContext;
+    
+    protected void setUp(){
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+    
+    protected String mapToJson(Object obj) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(obj);
+    }
+    
+    protected <T> T mapFromJson(String json,Class<T> clazz) throws JsonParseException,JsonMappingException,IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(json, clazz);
+    }
+}
