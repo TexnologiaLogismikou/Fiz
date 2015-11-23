5b8f0a7b15a595a55952379be90de87cdd4c72bc
newbi12 <shinigamidxd@gmail.com>
2015-11-23 21:20:27 +0200

    Issue : UploadImageV2
    Images are stored in local folder

--- src/main/webapp/scripts/ImgScript.js
+++ src/main/webapp/scripts/ImgScript.js
@@ -3,41 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//
-//
-//
-//function uploadImage(){
-//    document.getElementById("images").disabled = true;
-//   var images = document.getElementById("images");
-//   var imName = document.getElementById("filename").value;
-//     
-//    $.ajax({
-//        type : "post", 
-//        url  : "/upload/a", 
-//        //accept: "application/json",
-//        //contentType: "application/json",
-//        data : {"name":imName
-//               ,"file":images.src},
-//                
-//         success : function() { 
-//            //document.getElementById("images").value = null;//clears the input field "images"
-//            document.getElementById("images").value = "";
-//            document.getElementById("filename").value = "";
-//            alert("success");
-//        },
-//        
-//         error : function(response,e,data) { //if the program responds with fail-error this part of the code will run
-//            switch(data.toString()){
-//                case "Not Found" ://posts a specified alert on the problem
-//                    alert("images where wrong");
-//                    break;
-//                default://posts a general alert 
-//                    alert("Unexpected Error \n" + data.toString());
-//                    break;
-//            }
-//        }
-//    });
-//}
 
     function downloadImageWithPost(){
         var imgID = document.getElementById("imgID").value;
