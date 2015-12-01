07eaca91081e7f143bfb6787e4dde86fc408427d
fireandrew <fire-andrew@hotmail.com>
2015-12-01 22:48:44 +0200

    implement cors for REST api

---
+++ src/main/java/com/tech/configurations/WebConfig.java
@@ -0,0 +1,21 @@
+package com.tech.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by Andreas on 1/12/2015.
+ */
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost")
+                .allowedMethods("POST", "GET");
+    }
+}
