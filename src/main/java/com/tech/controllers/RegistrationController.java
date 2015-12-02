53159bd093ac14cb62e26f9b40b6dbd414bdc70a
fireandrew <fire-andrew@hotmail.com>
2015-12-03 00:41:45 +0200

    Master : register origs set to ip

--- src/main/java/com/tech/controllers/RegistrationController.java
+++ src/main/java/com/tech/controllers/RegistrationController.java
@@ -9,13 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
 
+@CrossOrigin(origins = "http://83.212.105.54")
 @RestController
 @RequestMapping("/register")
 public class RegistrationController {
