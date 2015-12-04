c249488d384718c3546a94853bae6d7cf79306b8
Alodapos <kazi666@hotmail.gr>
2015-12-04 09:13:51 +0200

    Master : Implement Login

---
+++ src/main/java/com/tech/controllers/LogInController.java
@@ -0,0 +1,41 @@
+package com.tech.controllers;
+
+import com.tech.configurations.tools.Host;
+import com.tech.controllers.superclass.BaseController;
+import com.tech.models.dtos.LoginUserDTO;
+import com.tech.models.entities.User;
+import com.tech.services.interfaces.IUserService;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = Host.apache)
+@RestController
+@RequestMapping("/login")
+public class LogInController extends BaseController {
+
+    @Autowired
+    IUserService service;
+
+    @RequestMapping(method = RequestMethod.POST)
+    public HttpEntity<JSONObject> login(@RequestBody LoginUserDTO loginUserDTO) {
+
+        JSONObject object = new JSONObject();
+        System.out.println(loginUserDTO.getUsername());
+        if (service.validateUser(loginUserDTO.getUsername(), loginUserDTO.getPassword())) {
+            User user = service.getUserByUsername(loginUserDTO.getUsername());
+
+            object.put("username", user.getUsername());
+            object.put("role", "ROLE_USER");
+            object.put("error","success");
+            System.out.println(object);
+            return new ResponseEntity<>(object,HttpStatus.OK);
+        }
+        object.put("error","error");
+        System.out.println(object);
+        return new ResponseEntity<>(object,HttpStatus.OK);
+    }
+}
