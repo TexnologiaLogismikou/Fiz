36aa18246b1cec667bd3f046962ebb54a8dcd9f8
milenaAz <milena.14@windowslive.com>
2015-12-05 15:45:47 +0200

    Dev-Issue-Timestamp-On-Friendlist : Refactor TestFriendController 2 Test added

--- src/test/java/com/tech/controllers/FriendControllerTest.java
+++ src/test/java/com/tech/controllers/FriendControllerTest.java
@@ -121,6 +121,7 @@ public class FriendControllerTest extends AbstractControllerTest
         
         when(userService.getUserByUsername("milena")).thenReturn(new User(1L,"milena","milena",true));
         when(userService.getUserByUsername("mixalis")).thenReturn(new User(3L,"mixalis","mixalis",true));
+        when(friendService.checkFriendIfExists(1L,3L)).thenReturn(true);
         
         MvcResult result = mvc.perform(MockMvcRequestBuilders.post(uri + "/deletefriend")
                 .content(json.toString())
@@ -163,6 +164,35 @@ public class FriendControllerTest extends AbstractControllerTest
         
         Assert.assertTrue("failure - expected HTTP response body to be '" + Responses.STRING_INAPPROPRIATE_FORMAT.getData() + "'",
                     content.equals(Responses.STRING_INAPPROPRIATE_FORMAT.getData()));
+        
+    }
+    
+    @Test
+    @Sql(scripts = "classpath:populateDB.sql")
+    public void testFriendNotExist() throws Exception 
+    {
+       json.put("username","milena");
+       json.put("friendname","vasilis"); 
+       
+       when(userService.checkUsername("vasilis")).thenReturn(false);
+       
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post(uri + "/addfriend")
+                .content(json.toString())
+                .contentType(MediaType.APPLICATION_JSON))
+                .andReturn();
+        
+        String content = result.getResponse().getContentAsString();
+        int status = result.getResponse().getStatus();
+        
+        verify(userService,times(0)).getUserByUsername(anyString());
+        verify(userService,times(1)).checkUsername(anyString());
+        verify(friendService,times(0)).checkFriendIfExists(anyLong(),anyLong());
+        verify(friendService,times(0)).addFriend(any(Friend.class));
+        
+        Assert.assertEquals("failure - expected HTTP status to be '404'", 404, status); 
+        
+        Assert.assertTrue("failure - expected HTTP response body to be '" + Responses.NOT_AVAILABLE.getData() + "'",
+                    content.equals(Responses.NOT_AVAILABLE.getData()));
         
     }
 }
