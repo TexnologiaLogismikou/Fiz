02ebf56d24b435b5617a931e8a4542d3b6efb959
newbi12 <shinigamidxd@gmail.com>
2015-12-06 16:26:26 +0200

    BigUpdate #step1 -> PopulateDB.sql

--- src/test/resources/populateDB.sql
+++ src/test/resources/populateDB.sql
@@ -7,24 +7,35 @@
  * Author:  KuroiTenshi
  * Created: Nov 15, 2015
  */
-delete from user_roles;
-delete from friendlist;
-delete from images;
-delete from usersdata;
-delete from messages;
-delete from user_info;
+DELETE FROM user_roles;
+DELETE FROM friendlist;
+DELETE FROM chatrooms;
+DELETE FROM images;
+DELETE FROM usersdata;
+DELETE FROM messages;
+DELETE FROM user_info;
 
-insert into usersdata  values (1,'milena','milena',true);
-insert into usersdata values (2,'iwanna','iwanna',true);
-insert into usersdata values (3,'mixalis','mixalis',true);
+INSERT INTO usersdata  VALUES (1,'milenaAz','milena',TRUE); --table#1
+INSERT INTO usersdata VALUES (2,'iwannaFot','iwanna',TRUE); --table#1
+INSERT INTO usersdata VALUES (3,'mixalisMix','mixalis',TRUE); --table#1
 
-insert into messages  values (1, 1, 'Hi');
-insert into messages values (2, 1, 'Hello, how are you');
-insert into messages values (3, 2, 'I m fine thank you');
+INSERT INTO user_roles VALUES(1,'ROLE_USER'); --table#2
+INSERT INTO user_roles VALUES(2,'ROLE_USER'); --table#2
+INSERT INTO user_roles VALUES(3,'ROLE_USER'); --table#2
 
-insert into friendlist  values (1,2);
-insert into friendlist  values (1,3);
+INSERT INTO user_info VALUES (1,'milena','Azwidou','22/11/1994','milena@gmail.com','Status','C:\FizData\DefaultImages\NoImage\noImg.jpg','kabala');  --table#3
+INSERT INTO user_info VALUES (2,'Iwanna','Fwtiadou','23/01/1994','iwanna@gmail.com','Status','C:\FizData\DefaultImages\NoImage\noImg.jpg','serres');  --table#3
+INSERT INTO user_info VALUES (3,'Mixalis','Mixailidis','17/10/1994','mixalis@gmail.com','Status','C:\FizData\DefaultImages\NoImage\noImg.jpg','thessaloniki');  --table#3
 
-insert into user_info values (1,'milena@gmail.com','C:\FizData\DefaultImages\NoImage\noImg.jpg','Status','Azwidou','22/11/1994','kabala','kabala'); 
-insert into user_info values(2,'iwanna@gmail.com','C:\FizData\DefaultImages\NoImage\noImg.jpg','Status','Fwtiadou','23/01/1994','serres','serres');
---insert into user_info values (3,'mixalis@gmail.com','C:\FizData\DefaultImages\NoImage\noImg.jpg','Status','mixailidis','17/10/1994','thessaloniki');
+INSERT INTO friendlist VALUES (1, 2,TO_TIMESTAMP('16-05-2011 12:00:00', 'dd-mm-yyyy hh24:mi:ss')); --table#5
+INSERT INTO friendlist VALUES (1, 3,TO_TIMESTAMP('16-05-2011 13:00:00', 'dd-mm-yyyy hh24:mi:ss')); --table#5
+
+INSERT INTO chatrooms VALUES (1,'testing room','1','1'); --table#7
+INSERT INTO chatrooms VALUES (1,'testing room','1','2'); --table#7
+INSERT INTO chatrooms VALUES (2,'second testing room','3','3'); --table#7
+INSERT INTO chatrooms VALUES (2,'second testing room','3','2'); --table#7
+
+INSERT INTO messages  VALUES (1, 1,'initial message',TO_TIMESTAMP('16-05-2011 12:30:00', 'dd-mm-yyyy hh24:mi:ss'),'1'); --table#6
+INSERT INTO messages  VALUES (2, 2,'second message',TO_TIMESTAMP('16-05-2011 13:30:00', 'dd-mm-yyyy hh24:mi:ss'),'1'); --table#6
+INSERT INTO messages  VALUES (3, 2,'third message',TO_TIMESTAMP('16-05-2011 14:30:00', 'dd-mm-yyyy hh24:mi:ss'),'1'); --table#6
+INSERT INTO messages  VALUES (4, 3,'forth message',TO_TIMESTAMP('16-05-2011 13:30:00', 'dd-mm-yyyy hh24:mi:ss'),'2'); --table#6
