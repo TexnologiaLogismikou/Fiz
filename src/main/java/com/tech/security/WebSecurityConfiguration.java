613ab659dae4d0b2746efa4157fbcaabf4fec2e8
Alodapos <kazi666@hotmail.gr>
2015-12-08 22:58:12 +0200

    Dev-Security : Security added

---
+++ src/main/java/com/tech/security/WebSecurityConfiguration.java
@@ -0,0 +1,55 @@
+package com.tech.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    AuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    AuthenticationEntryPoint restAuthenticationEntryPoint;
+
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password,enabled from usersdata where username=?")
+                .authoritiesByUsernameQuery(
+                        "select username, role from user_roles where username=?");
+    }
+
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                    .authorizeRequests()
+                        .antMatchers("/user**","/user/**").authenticated()
+                .and()
+                    .formLogin()
+                        .loginProcessingUrl("/j_spring_security_check")
+                        .usernameParameter("j_username")
+                        .passwordParameter("j_password")
+                        .successHandler(authenticationSuccessHandler)
+                        .failureHandler(new SimpleUrlAuthenticationFailureHandler())
+                .and()
+                    .logout();
+    }
+}
