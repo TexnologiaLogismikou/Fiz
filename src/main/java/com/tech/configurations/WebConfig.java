5f7013c79ea76f9d74998c895276afc33b6936d2
Alodapos <kazi666@hotmail.gr>
2015-12-17 01:48:00 +0200

    security : cors

--- src/main/java/com/tech/configurations/WebConfig.java
+++ src/main/java/com/tech/configurations/WebConfig.java
@@ -1,25 +1,25 @@
-package com.tech.configurations;
-
-import com.tech.configurations.tools.Host;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-/**
- * Created by Andreas on 1/12/2015.
- */
-@Configuration
-@EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(Host.apache)
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "accept", "X-Ajax-Call","Access-Control-Request-Method","Access-Control-Request-Headers")
-                .allowCredentials(true)
-                .exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
-    }
-}
+//package com.tech.configurations;
+//
+//import com.tech.configurations.tools.Host;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//
+///**
+// * Created by Andreas on 1/12/2015.
+// */
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig extends WebMvcConfigurerAdapter {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins(Host.apache)
+//                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+//                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "accept", "X-Ajax-Call","Access-Control-Request-Method","Access-Control-Request-Headers")
+//                .allowCredentials(true)
+//                .exposedHeaders("Access-Control-Allow-Origin","Access-Control-Allow-Credentials");
+//    }
+//}
