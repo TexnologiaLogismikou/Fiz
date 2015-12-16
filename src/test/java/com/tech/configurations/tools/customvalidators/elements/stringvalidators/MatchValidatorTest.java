d87ea23bf6e3a3856b1d33aceae620dcd51b5886
George Margaritis <geormarga@gmail.com>
2015-12-16 23:03:02 +0200

    Test-Validator Tests for MatchValidator, NotMatchValidator and EmptyNumberValidator

--- src/test/java/com/tech/configurations/tools/customvalidators/elements/stringvalidators/MatchValidatorTest.java
+++ src/test/java/com/tech/configurations/tools/customvalidators/elements/stringvalidators/MatchValidatorTest.java
@@ -18,35 +18,55 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 /**
- *
  * @author KuroiTenshi
  */
 public class MatchValidatorTest {
-    
+
     public MatchValidatorTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
-    public void testValidate() {
+    public void testValidateSuccess() {
+
+        String test = "testString";
+        IStringValidator stringValidator = new MatchValidator("/testString/");
+        Pair<Boolean, ResponseEntity> answer = stringValidator.validate(test);
+
+        Assert.assertEquals("Failure - expected true but the answer was false", Boolean.TRUE, answer.getLeft());
+        Assert.assertEquals("Failure - expected '" + Responses.SUCCESS.getData() + "' but the answer was '" + answer.getRight().getBody() + "'",
+                answer.getRight(), new ResponseEntity<>(Responses.SUCCESS, HttpStatus.OK));
+
+
     }
-    
+
     @Test
     public void testValidateFail() {
+
+        String test = "testString";
+        IStringValidator stringValidator = new MatchValidator("");
+        Pair<Boolean, ResponseEntity> answer = stringValidator.validate(test);
+
+        Assert.assertNotEquals("Failure - expected false but the answer was true", Boolean.TRUE, answer.getLeft());
+        Assert.assertEquals("Failure - expected '" + Responses.STRING_INAPPROPRIATE_FORMAT.getData() + "' but the answer was '" + answer.getRight().getBody() + "'",
+                answer.getRight(), new ResponseEntity<>(Responses.STRING_INAPPROPRIATE_FORMAT, HttpStatus.NOT_ACCEPTABLE));
+
+
     }
+
 }
