73e4f5cbe0407ac618b105da5040f2bd9b92eb28
fireandrew <fire-andrew@hotmail.com>
2015-12-17 06:29:49 +0200

    Dev-security : host to ip

--- src/main/java/com/tech/controllers/RegistrationController.java
+++ src/main/java/com/tech/controllers/RegistrationController.java
@@ -18,6 +18,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = Host.apache)
 @RestController
 @RequestMapping("/register")
 public class RegistrationController extends BaseController{
