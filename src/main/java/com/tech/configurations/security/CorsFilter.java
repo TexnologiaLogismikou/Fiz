85dc7ce4279a3533243ff3928088995fb5836fde
Alodapos <kazi666@hotmail.gr>
2015-12-17 04:29:04 +0200

    security : cors change

--- src/main/java/com/tech/configurations/security/CorsFilter.java
+++ src/main/java/com/tech/configurations/security/CorsFilter.java
@@ -20,6 +20,7 @@ public class CorsFilter extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Headers", "X-Ajax-call");
         response.setHeader("Access-Control-Allow-Headers", "X-requested-with");
         response.setHeader("Access-Control-Expose-Headers", "x-requested-with");
+        response.setHeader("Access-Control-Expose-Headers", "Origin");
         filterChain.doFilter(request, response);
     }
 }
