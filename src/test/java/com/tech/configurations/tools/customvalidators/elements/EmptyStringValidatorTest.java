daeee4fe6ec4512027fb301374228bb8f84b39a5
BillBasilis <bill54greek@gmail.com>
2015-12-17 02:50:38 +0200

    Dev-Issue-Implementing-Testing-Vas Tests made NotEmptyValidatorSTest 	EmptyStringValidator DTO made ChatroomLocationDTO 	ChatroomLocationUpdateDTO 	ChatroomMemberDTO

--- src/test/java/com/tech/configurations/tools/customvalidators/elements/EmptyStringValidatorTest.java
+++ src/test/java/com/tech/configurations/tools/customvalidators/elements/EmptyStringValidatorTest.java
@@ -5,12 +5,18 @@
  */
 package com.tech.configurations.tools.customvalidators.elements;
 
+import com.tech.configurations.tools.Pair;
+import com.tech.configurations.tools.Responses;
+import com.tech.configurations.tools.customvalidators.elements.stringvalidators.NotEmptyValidatorS;
+import com.tech.configurations.tools.customvalidators.interfaces.IStringValidator;
+import junit.framework.Assert;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import static org.junit.Assert.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 /**
  *
@@ -38,7 +44,31 @@ public class EmptyStringValidatorTest {
     }
 
     @Test
-    public void testValidate() {
+    public void testValidateNotEmpty() {
+        
+        IStringValidator stringValidator = new EmptyStringValidator();
+        
+        String str = null;
+        
+        Pair<Boolean,ResponseEntity> responce = stringValidator.validate(str);
+        
+        Assert.assertEquals("Failure - expected False but the answer was False",responce.getLeft(), Boolean.FALSE);
+        Assert.assertEquals("failure - returned response was '" + responce.getRight() + " '",
+                responce.getRight(), new ResponseEntity<>(Responses.STRING_INAPPROPRIATE_FORMAT, HttpStatus.NOT_ACCEPTABLE)); 
+    }
+    
+    @Test
+    public void testValidateNotNull() {
+        
+        IStringValidator stringValidator = new EmptyStringValidator();
+        
+        String str = "Billaras";
+        
+        Pair<Boolean,ResponseEntity> responce = stringValidator.validate(str);
+        
+        Assert.assertEquals("Failure - expected True but the answer was False",responce.getLeft(), Boolean.TRUE);
+        Assert.assertEquals("Failure - expected '"+Responses.SUCCESS.getData()+"' but the answer was '"+responce.getRight().getBody()+"'",
+            responce.getRight(), new ResponseEntity<>(Responses.SUCCESS,HttpStatus.OK));
     }
     
 }
