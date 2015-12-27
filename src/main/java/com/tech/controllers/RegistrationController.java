7d765c62b92fdc0568b67345f1de2efba55f3af7
newbi12 <shinigamidxd@gmail.com>
2015-12-27 19:16:44 +0200

    Dev-Issue-TestForMessageController : Bugfixing

--- src/main/java/com/tech/controllers/RegistrationController.java
+++ src/main/java/com/tech/controllers/RegistrationController.java
@@ -16,10 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = Host.apache)
-@RestController
+@Controller
 @RequestMapping("/register")
 public class RegistrationController extends BaseController{
 
