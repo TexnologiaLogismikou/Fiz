d5880696c644921d3b617bf61050a1cf9bc72216
fireandrew <fire-andrew@hotmail.com>
2016-01-22 07:07:20 +0200

    Dev-Issue-Auth0 : cors *

--- src/main/java/com/tech/controllers/LoginController.java
+++ src/main/java/com/tech/controllers/LoginController.java
@@ -14,7 +14,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = Host.apache)
+@CrossOrigin
 @RestController
 @RequestMapping("/login")
 public class LoginController {
